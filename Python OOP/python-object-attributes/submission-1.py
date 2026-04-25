8c3c49042903fae8d576524fd8523f80293a0d65
Isaac Adjei <offices.isaac@gmail.com>
2026-04-25 18:20:10 +0100

    Add: python-object-attributes - submission-1

---
+++ Python OOP/python-object-attributes/submission-1.py
@@ -0,0 +1,17 @@
+class Pet:
+    def __init__(self, name: str, species: str, hunger: int, energy: int):
+        self.name = name
+        self.species = species
+        self.hunger = hunger
+        self.energy = energy
+
+whiskers = Pet("Whiskers", "cat", 6, 8)
+
+# TODO: Print Whiskers' initial attributes
+print(whiskers.name, whiskers.species, whiskers.hunger, whiskers.energy)
+
+# TODO: Modify Whiskers' attributes:
+#  - Decrease hunger by 3
+#  - Increase energy by 2
+
+# TODO: Print Whiskers' modified attributes
