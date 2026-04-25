f8833638e738cfbfccf371a9be2849c5299fb313
Isaac Adjei <offices.isaac@gmail.com>
2026-04-25 17:24:50 +0100

    Add: python-printing-text - submission-8

---
+++ Python For Beginners/python-printing-text/submission-8.py
@@ -0,0 +1,8 @@
+# Method 1
+#print('My favorite quote is "To be or not to be."')
+
+# Method 2
+#print("""My favorite quote is "To be or not to be.""")
+
+# Method 3
+print("My favorite quote is \"To be or not to be.\"")
