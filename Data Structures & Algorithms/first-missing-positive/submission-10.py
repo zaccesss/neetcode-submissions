9957a73b345cfb5dd02e465bf115c00817953fe3
Isaac Adjei <offices.isaac@gmail.com>
2026-06-12 03:08:29 +0100

    Add: first-missing-positive - submission-10

---
+++ Data Structures & Algorithms/first-missing-positive/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def firstMissingPositive(
+        self,
+        nums: List[int]
+    ) -> int:
+
+        n = len(nums)
+
+        # Placed each valid number into its correct index.
+        for i in range(n):
+
+            while (
+                nums[i] > 0
+                and nums[i] <= n
+                and nums[i]
+                != nums[nums[i] - 1]
+            ):
+
+                j = nums[i] - 1
+
+                nums[i], nums[j] = (
+                    nums[j],
+                    nums[i]
+                )
+
+        # Found first missing positive.
+        for i in range(n):
+
+            if nums[i] != i + 1:
+
+                return i + 1
+
+        # All numbers 1..n exist.
+        return n + 1
