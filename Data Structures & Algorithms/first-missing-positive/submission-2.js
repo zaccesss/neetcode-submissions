3c9777a1ba09c20a4908a66a79bea777d6d72e4d
Isaac Adjei <offices.isaac@gmail.com>
2026-06-12 03:04:17 +0100

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    firstMissingPositive(nums) {
+
+        const n = nums.length;
+
+        // Placed each valid number into its correct index.
+        for (let i = 0; i < n; i++) {
+
+            while (
+                nums[i] > 0 &&
+                nums[i] <= n &&
+                nums[i] !== nums[nums[i] - 1]
+            ) {
+
+                const correctIndex =
+                    nums[i] - 1;
+
+                [
+                    nums[i],
+                    nums[correctIndex]
+                ] = [
+                    nums[correctIndex],
+                    nums[i]
+                ];
+            }
+        }
+
+        // Found first missing positive.
+        for (let i = 0; i < n; i++) {
+
+            if (nums[i] !== i + 1) {
+
+                return i + 1;
+            }
+        }
+
+        // All numbers 1..n exist.
+        return n + 1;
+    }
+}
